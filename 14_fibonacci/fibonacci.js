8b5cb5e488e9b2ea2cab9a51b0ba0533683358a2
MaazMohammedM <maazmohammed742@gmail.com>
2025-11-06 14:23:39 +0530

    add solution for fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (+n === 0) {
+        return 0
+    }
 
+    if(+n < 0){
+        return 'OOPS'
+    }
+    
+    let arr = [0, 1];
+    for (let i = 2; i <= +n; i++) {
+        arr.push(arr[i - 2] + arr[i - 1])
+    }
+    return arr.at(-1)
 };
 
 // Do not edit below this line
