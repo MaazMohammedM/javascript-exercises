d8837330ee393bc9b720266e7647a423857cba20
MaazMohammedM <maazmohammed742@gmail.com>
2025-11-06 08:13:24 +0530

    add solution for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedStr = string.replace(/[^a-zA-z0-9]/g, "").toLowerCase();
+    let reversedStr = string.replace(/[^a-zA-z0-9]/g, "").toLowerCase().split('').reverse().join('');
+    return cleanedStr === reversedStr ? true : false
 };
 
 // Do not edit below this line
