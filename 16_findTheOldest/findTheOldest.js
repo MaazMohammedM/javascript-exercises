156a5eb717106e96a4420ca75559af6c370a8941
MaazMohammedM <maazmohammed742@gmail.com>
2025-11-06 15:41:01 +0530

    add solution for findtheoldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,9 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+let currentYear = new Date().getFullYear();
+
+
+return arr.reduce((oldest,current)=>((oldest.yearOfDeath || currentYear) - oldest.yearOfBirth)>((current.yearOfDeath || 2025) - current.yearOfBirth) ? oldest : current);
 
 };
 
